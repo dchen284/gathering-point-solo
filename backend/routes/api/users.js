f8e9dfe9752a19b4f1b4f39a4dcc53dd069291f7
Danny <dchen284@gmail.com>
2021-05-20 12:47:52 -0500

    tested signup route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,24 @@
+//external require/imports
 const express = require('express')
 const router = express.Router();
+const asyncHandler = require('express-async-handler');
+//internal require/imports
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+// Sign up
+router.post(
+    '/',
+    asyncHandler(async (req, res) => {
+      const { email, password, username } = req.body;
+      const user = await User.signup({ email, username, password });
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user,
+      });
+    }),
+  );
 
 module.exports = router;
