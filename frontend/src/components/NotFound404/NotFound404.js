5ed145a2d195d5ddc994e856e643f749f7da1b10
Danny <dchen284@gmail.com>
2021-08-02 15:04:12 -0500

    add functionality to NotFound404 component

--- frontend/src/components/NotFound404/NotFound404.js
+++ frontend/src/components/NotFound404/NotFound404.js
@@ -1,16 +1,64 @@
+//External imports
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+//Internal imports
+import { Modal } from '../../context/Modal';
+import EventForm from '../EventFormModal/EventForm';
+import LoginForm from '../LoginFormModal/LoginForm';
 import './NotFound404.css';
 
 export default function NotFound404() {
+
+    //hooks
+    const history = useHistory();
+    const [showModal, setShowModal] = useState(false);
+    const [modalType, setModalType] = useState('');
+    const sessionUser = useSelector( (state) => state.session.user );
+
+    //functions
+    function clickCreateAnEvent() {
+        sessionUser ? setModalType('Create') : setModalType('Login');
+        setShowModal(true);
+    }
+
+    console.log('++++++++++++', modalType)
+
+    //JSX
     return (
         <div className='not-found-container'>
-            <p className='not-found--icon'><i class="fas fa-exclamation"></i></p>
+            <p className='not-found--icon'><i className="fas fa-exclamation"></i></p>
             <p className='not-found--whoops'>
                 Whoops, the page or event you are looking for was not found.
             </p>
             <p className='not-found--directions'>
                 If you feel this message is in error, please let us know.
             </p>
-            
+            <div className='not-found--buttons'>
+                <button
+                className='btn-primary'
+                onClick={clickCreateAnEvent}
+                >
+                    Create An Event
+                </button>
+                {showModal && modalType === 'Create' && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <EventForm formAction='Create' setShowModal={setShowModal}/>
+                </Modal>
+                )}
+                {showModal && modalType === 'Login' && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <LoginForm setShowModal={setShowModal}/>
+                </Modal>
+                )}
+                <button
+                className='btn-secondary'
+                onClick={()=>{history.push('/')}}
+                >
+                    Find An Event
+                </button>
+            </div>
+
         </div>
     )
 }
